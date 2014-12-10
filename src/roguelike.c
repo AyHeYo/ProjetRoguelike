137f51776dd11805ea3218248cdf81c8fbca8fb4
Youss <Youssef.Lamniy.Etu@univ-lemans.fr>
2014-12-10 12:58:46 +0100

    Fonction generation avancée.

--- src/roguelike.c
+++ src/roguelike.c
@@ -48,7 +48,7 @@ Dimension * get_maze_dimension() {
 	return &maze_dimension;
 }
 
-void generate_maze() {//fonction de génération via un fichier pour continuer le développement des autres fonctionnalité en attendant la vraie fonction de génération aléatoire
+/*void generate_maze() {//fonction de génération via un fichier pour continuer le développement des autres fonctionnalité en attendant la vraie fonction de génération aléatoire
 	int i;
 	FILE * file = fopen("level.txt", "r");
 	free(maze);//libération de l'espace mémoire alloué au labyrinthe, inutile de vérifier si NULL car free(NULL) n'a tout simplement aucun effet
@@ -65,13 +65,46 @@ void generate_maze() {//fonction de génération via un fichier pour continuer l
 	fclose(file);
 	player_location.line = player_location.row = 2;//placement du joueur, pour l'instant arbitrairement à 2:2
 	maze[player_location.line * maze_dimension.horizontal + player_location.row] = PLAYER;
-}
+}*/
 
-/*void generate_maze() {//vraie fonction de génération à finir d'implémenter
-	int i;
+void generate_maze() {//vraie fonction de génération à finir d'implémenter
+	int rang, col, i;
 	free(maze);//libération de l'espace mémoire alloué au labyrinthe, inutile de vérifier si NULL car free(NULL) n'a tout simplement aucun effet
 	maze = (Square *) malloc(20 * 20 * sizeof(Square));//allocation de la mémoire nécessaire pour stocker le labyrinthe
 	maze_dimension.horizontal = maze_dimension.vertical = 20;//sélection des dimensions du labyrinthe, pour l'instant constante à 20
+	
+
+  //  Initialisation de toutes les positions partant du coté gauche du mur
+    for ( rang = 0; rang < maze_dimension.vertical ; rang++ )
+    {
+	 for (col = 0; col < maze_dimension.horizontal; col++)
+        {
+            printf("%2c",maze[rang * maze_dimension.horizontal + col]);
+        }
+        maze[rang * maze_dimension.horizontal + col] = WALL;
+ 		maze[rang * maze_dimension.horizontal + maze_dimension.horizontal-1] = WALL;
+    }
+
+    rang = rand() % 19 + 1;
+    maze[rang * maze_dimension.horizontal] = WALL;
+
+    rang = rand() % 19 + 1;
+    maze[rang * maze_dimension.horizontal + maze_dimension.horizontal-1] = WALL;
+    for (col = 1; col < maze_dimension.horizontal - 1; col++)
+    {
+        maze[col] = WALL;
+        maze[(maze_dimension.horizontal-1) * maze_dimension.horizontal + col] = WALL;
+    }
+    for (rang = 0; rang < maze_dimension.vertical; rang++)
+    {
+        for (col = 0; col < maze_dimension.horizontal; col++)
+        {
+            printf("%2c",maze[rang * maze_dimension.horizontal + col]);
+        }
+        puts("");
+    }
+    puts("");
+
 	for (i = 0 ; i < maze_dimension.horizontal * maze_dimension.vertical ; i++) {//remplissage du labyrinthe par des murs
 		maze[i] = WALL;
 	}
@@ -80,7 +113,7 @@ void generate_maze() {//fonction de génération via un fichier pour continuer l
 	//génération des couloirs
 	player_location.line = player_location.row = 2;//placement du joueur, pour l'instant arbitrairement à 2:2
 	maze[player_location.line * maze_dimension.horizontal + player_location.row] = PLAYER;
-}*/
+}
 
 Location * get_player_location() {
 	return &player_location;
@@ -119,38 +152,3 @@ boolean player_can_move(Direction direction) {
 			return false;
 	}
 }
-
-/*Rectangle lab_dim = {0, 0, TAILLE - 1, TAILLE - 1};
-
-int random2(int a, int b) {
-	b +=1;
-	return rand() % (b - a) + a;
-}
-
-void generer() {//la fonction de génération devient la fonction generate_maze(), continuer ou recopier le code à l'intérieur
-	int i, j, n_salles, s;
-	Rectangle * salles;
-	for (i = 0 ; i < TAILLE ; i++) {
-		for (j = 0 ; j < TAILLE ; j++) {
-			labyrinthe[i][j] = MUR;
-		}
-	}
-	n_salles = random2(3, lab_dim.l2 / 5);
-	salles = (Rectangle *) malloc(n_salles * sizeof(Rectangle));
-	if (random2(0, 1)) {
-		salles[0].l1 = lab_dim.l1 + 1;
-		salles[0].c1 = random2(lab_dim.c1 + 1, lab_dim.c2 / 4);
-	} else {
-		salles[0].l1 = random2(lab_dim.l1 + 1, lab_dim.l2 / 4);
-		salles[0].c1 = lab_dim.c1 + 1;
-	}
-	salles[0].l2 = random2(salles[0].l1 + 4, salles[0].l1 + lab_dim.l2 / 3);
-	salles[0].c2 = random2(salles[0].c1 + 4, salles[0].c1 + lab_dim.c2 / 3);
-	for (s = 0 ; s <= 0 ; s++) {
-		for (i = salles[0].l1 ; i < salles[0].l2 ; i++) {
-			for (j = salles[0].c1 ; j < salles[0].c2 ; j++) {
-				labyrinthe[i][j] = VIDE;
-			}
-		}
-	}
-}*/
